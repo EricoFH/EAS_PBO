a31b9dbde54aede046bf6416e7a103038a05e693
MarifYu <andreanhirata0@gmail.com>
2026-06-30 12:47:27 +0700

    penambahan efek
    menambahan low HP triggers untuk goblin, skeleton, minotaur, dan naga

--- enemy.py
+++ enemy.py
@@ -7,7 +7,13 @@ goblin = {
     "drops": [
         {"item": "Goblin Ear", "type": "material", "chance": 80},
         {"item": "Potion", "type": "consumable", "chance": 35},
-    ]
+    ],
+    "low_hp_trigger": {
+        "threshold": 30,        # efek yang kita kasih persen HP
+        "type": "panic",        # efek yang kita kasih dodge turun drastis
+        "dodge_penalty": 15,
+        "message": "Goblin panik dan mulai gemetar ketakutan!"
+    }
 }
 
 skeleton = {
@@ -19,7 +25,13 @@ skeleton = {
     "drops": [
         {"item": "Bone Fragment", "type": "material", "chance": 80},
         {"item": "Potion", "type": "consumable", "chance": 40},
-    ]
+    ],
+    "low_hp_trigger": {
+        "threshold": 30,
+        "type": "enrage",       # buff efek attack naik
+        "attack_boost": 4,
+        "message": "Skeleton mengamuk, tulangnya berderak hebat!"
+    }
 }
 
 minotaur = {
@@ -31,7 +43,13 @@ minotaur = {
     "drops": [
         {"item": "Minotaur Horn", "type": "material", "chance": 70},
         {"item": "Potion", "type": "consumable", "chance": 50},
-    ]
+    ],
+    "low_hp_trigger": {
+        "threshold": 25,
+        "type": "self_heal",    # efek heal sekali pakai
+        "heal_amount": 10,
+        "message": "Minotaur meraung dan memulihkan sebagian lukanya!"
+    }
 }
 
 naga = {
@@ -43,7 +61,15 @@ naga = {
     "drops": [
         {"item": "Red Horn", "type": "material", "chance": 70},
         {"item": "Potion", "type": "consumable", "chance": 100},
-    ]
+    ],
+    "low_hp_trigger": {
+        "threshold": 20,
+        "type": "enrage_poison",  # buff khusus attack naik + poison ke player
+        "attack_boost": 6,
+        "poison_damage": 4,
+        "poison_turns": 3,
+        "message": "Crimson Dragon mengamuk dan menyemburkan racun mematikan!"
+    }
 }
 enemy_pool = [goblin, skeleton, minotaur]
 boss_pool = [naga]
